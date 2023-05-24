e50e9fdc587ed1f9d6a9ba50d894c35da4e68881
Wyatt <Wyatt6@163.com>
2023-05-24 17:29:01 +0800

    服务监控中心

---
+++ basic-monitor/src/main/java/run/wyatt/oneplatform/monitor/BasicMonitorApplication.java
@@ -0,0 +1,19 @@
+package run.wyatt.oneplatform.monitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Wyatt
+ * @date 2023/5/24 16:22
+ */
+@EnableAdminServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BasicMonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BasicMonitorApplication.class, args);
+    }
+}

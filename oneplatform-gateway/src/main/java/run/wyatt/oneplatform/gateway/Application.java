9c47f66cf229ddc56ada0d960a75c848cdb92fb6
Wyatt <Wyatt6@163.com>
2023-06-16 14:30:03 +0800

    改名

--- oneplatform-gateway/src/main/java/run/wyatt/oneplatform/gateway/Application.java
+++ oneplatform-gateway/src/main/java/run/wyatt/oneplatform/gateway/Application.java
@@ -10,8 +10,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-public class OneplatformGatewayApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(OneplatformGatewayApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }

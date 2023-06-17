d6b30ea82eaa4b685ce4323e54235ad72642ddbc
Wyatt <Wyatt6@163.com>
2023-06-17 16:45:39 +0800

    更新

--- oneplatform-monitor/src/main/java/run/wyatt/oneplatform/monitor/Application.java
+++ oneplatform-monitor/src/main/java/run/wyatt/oneplatform/monitor/Application.java
@@ -12,8 +12,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableAdminServer
 @EnableDiscoveryClient
 @SpringBootApplication
-public class OneplatformMonitorApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(OneplatformMonitorApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }

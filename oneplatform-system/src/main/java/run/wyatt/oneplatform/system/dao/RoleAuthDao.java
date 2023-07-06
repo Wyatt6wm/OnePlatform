4c7a5dff9bdd604e263130c160189ddb657f6689
Wyatt(laptop) <Wyatt6@163.com>
2023-07-06 13:17:33 +0800

    RoleAuthDao

--- oneplatform-system/src/main/java/run/wyatt/oneplatform/system/dao/RoleAuthDao.java
+++ oneplatform-system/src/main/java/run/wyatt/oneplatform/system/dao/RoleAuthDao.java
@@ -1,8 +1,15 @@
 package run.wyatt.oneplatform.system.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+
 /**
  * @author Wyatt
  * @date 2023/6/21 11:41
  */
-public class RoleAuthDao {
+@Mapper
+public interface RoleAuthDao {
+    long insert(Long roleId, Long authId);
+    long delete(Long roleId, Long authId);
+    long deleteByRoleId(Long roleId);
+    long deleteByAuthId(Long authId);
 }

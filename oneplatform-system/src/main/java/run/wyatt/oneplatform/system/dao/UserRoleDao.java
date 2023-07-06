9b5468bd8dfd1decd071524810ed257a936289d4
Wyatt(laptop) <Wyatt6@163.com>
2023-07-06 13:20:55 +0800

    UserRoleDao

--- oneplatform-system/src/main/java/run/wyatt/oneplatform/system/dao/UserRoleDao.java
+++ oneplatform-system/src/main/java/run/wyatt/oneplatform/system/dao/UserRoleDao.java
@@ -5,5 +5,8 @@ package run.wyatt.oneplatform.system.dao;
  * @date 2023/6/21 11:41
  */
 public interface UserRoleDao {
-    int insert(Long userId, Long roleId);
+    long insert(Long userId, Long roleId);
+    long delete(Long userId, Long roleId);
+    long deleteByUserId(Long userId);
+    long deleteByRoleId(Long roleId);
 }

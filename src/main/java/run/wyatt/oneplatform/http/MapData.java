b59dd54f0de62279d5c016229881e53dde0e2788
Wyatt(laptop) <Wyatt6@163.com>
2023-07-31 16:00:07 +0800

    响应数据封装

---
+++ src/main/java/run/wyatt/oneplatform/http/MapData.java
@@ -0,0 +1,18 @@
+package run.wyatt.oneplatform.http;
+
+import java.util.HashMap;
+
+/**
+ * @author Wyatt
+ * @date 2023/7/6 16:00
+ */
+public class MapData extends HashMap<String, Object> {
+    public MapData() {
+        super();
+    }
+
+    public MapData(String key, Object value) {
+        super();
+        this.put(key, value);
+    }
+}

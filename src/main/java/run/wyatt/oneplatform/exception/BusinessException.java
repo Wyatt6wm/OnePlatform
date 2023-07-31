de2df325364725711226120db9c4fbaf54f5cadc
Wyatt(laptop) <Wyatt6@163.com>
2023-07-31 15:59:42 +0800

    异常类

---
+++ src/main/java/run/wyatt/oneplatform/exception/BusinessException.java
@@ -0,0 +1,15 @@
+package run.wyatt.oneplatform.exception;
+
+/**
+ * @author Wyatt
+ * @date 2023/6/21 15:09
+ */
+public class BusinessException extends RuntimeException {
+    public BusinessException() {
+        super("业务逻辑处理错误");
+    }
+
+    public BusinessException(String mesg) {
+        super(mesg);
+    }
+}

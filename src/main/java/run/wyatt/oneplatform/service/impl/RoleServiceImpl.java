d5e37f9054b1c9b1b5af4b358162b58e0552b32d
Wyatt(laptop) <Wyatt6@163.com>
2023-08-03 16:48:45 +0800

    删除角色时同时删除与用户、与权限的关联关系

--- src/main/java/run/wyatt/oneplatform/service/impl/RoleServiceImpl.java
+++ src/main/java/run/wyatt/oneplatform/service/impl/RoleServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import run.wyatt.oneplatform.dao.AuthDao;
 import run.wyatt.oneplatform.dao.RoleAuthDao;
 import run.wyatt.oneplatform.dao.RoleDao;
+import run.wyatt.oneplatform.dao.UserRoleDao;
 import run.wyatt.oneplatform.model.constant.RedisConst;
 import run.wyatt.oneplatform.model.entity.Role;
 import run.wyatt.oneplatform.model.exception.BusinessException;
@@ -36,6 +37,8 @@ public class RoleServiceImpl implements RoleService {
     private AuthDao authDao;
     @Autowired
     private RoleAuthDao roleAuthDao;
+    @Autowired
+    private UserRoleDao userRoleDao;
     @Autowired
     private AuthService authService;
 
@@ -128,10 +131,12 @@ public class RoleServiceImpl implements RoleService {
 
         if (roleDao.delete(roleId) == 0) {
             throw new BusinessException("该角色数据不存在");
+        } else {
+            userRoleDao.deleteByRoleId(roleId);
+            roleAuthDao.deleteByRoleId(roleId);
+            updateRoleDbChangeTime();
+            log.info("成功删除角色记录");
         }
-
-        updateRoleDbChangeTime();
-        log.info("成功删除角色记录");
     }
 
     @Override
